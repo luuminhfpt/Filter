f2b7359f6394826942a5152044a10dfa33885db3
Trinh Le <trinh.le@bigin.vn>
2021-03-10 12:26:31 +0700

    update code

--- src/FilterServiceProvider.php
+++ src/FilterServiceProvider.php
@@ -13,11 +13,6 @@ class FilterServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'vdvt');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'vdvt');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
@@ -31,8 +26,6 @@ class FilterServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/filter.php', 'filter');
-
         // Register the service the package provides.
         $this->app->singleton('filter', function ($app) {
             return new Filter;
@@ -56,27 +49,6 @@ class FilterServiceProvider extends ServiceProvider
      */
     protected function bootForConsole(): void
     {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__.'/../config/filter.php' => config_path('filter.php'),
-        ], 'filter.config');
 
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/vdvt'),
-        ], 'filter.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/vdvt'),
-        ], 'filter.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/vdvt'),
-        ], 'filter.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
